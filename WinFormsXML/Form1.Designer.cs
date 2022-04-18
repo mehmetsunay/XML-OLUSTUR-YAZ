196d93c2df69f69801bfa907e34d408be6e9eca6
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-18 16:22:57 +0300

    XML-OLUSTUR-YAZ
    XML-OLUSTUR-YAZ

---
+++ WinFormsXML/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace WinFormsXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_soyisim = new System.Windows.Forms.TextBox();
+            this.btn_olustur = new System.Windows.Forms.Button();
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.lbl_soyisim = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(195, 107);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(125, 27);
+            this.txt_isim.TabIndex = 0;
+            // 
+            // txt_soyisim
+            // 
+            this.txt_soyisim.Location = new System.Drawing.Point(195, 177);
+            this.txt_soyisim.Name = "txt_soyisim";
+            this.txt_soyisim.Size = new System.Drawing.Size(125, 27);
+            this.txt_soyisim.TabIndex = 1;
+            // 
+            // btn_olustur
+            // 
+            this.btn_olustur.Location = new System.Drawing.Point(195, 249);
+            this.btn_olustur.Name = "btn_olustur";
+            this.btn_olustur.Size = new System.Drawing.Size(125, 29);
+            this.btn_olustur.TabIndex = 2;
+            this.btn_olustur.Text = "OLUŞTUR";
+            this.btn_olustur.UseVisualStyleBackColor = true;
+            this.btn_olustur.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_isim.Location = new System.Drawing.Point(41, 108);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(49, 20);
+            this.lbl_isim.TabIndex = 3;
+            this.lbl_isim.Text = "İSİM :";
+            // 
+            // lbl_soyisim
+            // 
+            this.lbl_soyisim.AutoSize = true;
+            this.lbl_soyisim.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_soyisim.Location = new System.Drawing.Point(41, 178);
+            this.lbl_soyisim.Name = "lbl_soyisim";
+            this.lbl_soyisim.Size = new System.Drawing.Size(77, 20);
+            this.lbl_soyisim.TabIndex = 4;
+            this.lbl_soyisim.Text = "SOYİSİM :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(456, 406);
+            this.Controls.Add(this.lbl_soyisim);
+            this.Controls.Add(this.lbl_isim);
+            this.Controls.Add(this.btn_olustur);
+            this.Controls.Add(this.txt_soyisim);
+            this.Controls.Add(this.txt_isim);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.TextBox txt_soyisim;
+        private System.Windows.Forms.Button btn_olustur;
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.Label lbl_soyisim;
+    }
+}
+
---
+++ WinFormsXML/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WinFormsXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {    // xml dosyamın oluşacak yolunu proje yolu olarak belirttim
+            string xmlpath = Application.StartupPath + "\\Customer.xml";
+            //Yazıcı nesnemi ve formatımı oluşturdum
+            XmlTextWriter customer = new XmlTextWriter(xmlpath, UTF8Encoding.UTF8);
+            //Format düzenleme satırımı ekledım
+            customer.Formatting = Formatting.Indented;
+            //ve işlemimi başlatıyorum
+             customer.WriteStartDocument();
+             customer.WriteStartElement("Müşteriler");
+             customer.WriteStartElement("Müşteri");
+            //Id Tanımlamak için kullandım 
+            customer.WriteStartAttribute("ID", "1");
+            //Sıra geldi Textboxlardan verileri almaya!!
+            customer.WriteElementString("isim", txt_isim.Text);
+            customer.WriteElementString("soyisim",txt_soyisim.Text);
+            //tag kapama işlemimi yapıyorum 
+            customer.WriteEndElement();
+            customer.WriteEndElement();
+            //Kapıyorum
+            customer.Close();
+            MessageBox.Show("İŞLEMİNİZ TAMAMLANMIŞTIR");
+            Application.Exit();
+
+
+        }
+    }
+}
